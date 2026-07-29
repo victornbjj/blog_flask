09466460cdbe15f24c3cadebd403b146e15651b3
J. Victor Pereira <victorifpb78@gmail.com>
2026-07-29 13:44:34 -0300

    feat: add password_hasd and email to user

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from src.extensions import db
 
@@ -9,9 +10,22 @@ class User(db.Model):
 
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    email:Mapped[str] = mapped_column(sa.String, nullable=False, unique=True)
+    password_hash = mapped_column(sa.String, nullable=False)
     active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="author")
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.username!r}, active={self.active!r})"
+        return f"User(id={self.id!r}, name={self.username!r}, active={self.active!r})"  
+    
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password: str):
+        if not self.password_hash:
+            return False
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self) -> str:
+            return f"User(id={self.id!r}, name={self.username!r}, active={self.active!r})" 
